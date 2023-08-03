9a24b8f48b472f58eccf766542aaca1403dff72e
Tanel Teinemaa <teinemaa@gmail.com>
2023-08-03 12:25:39 +0300

    resilience to latency

--- src/main/java/com/battlesnake/starter/Service.java
+++ src/main/java/com/battlesnake/starter/Service.java
@@ -5,13 +5,13 @@ import static com.battlesnake.starter.Move.*;
 public class Service {
     public Move move(MoveRequest request) {
         Coordinate head = request.you.head;
-        if (head.x == 0 && head.y < 10) {
+        if (head.x <= 1 && head.y < 9) {
             return UP;
         }
-        if (head.x < 10 && head.y == 10) {
+        if (head.x < 9 && head.y >= 9) {
             return RIGHT;
         }
-        if (head.x == 10 && head.y > 0) {
+        if (head.x >= 9 && head.y > 1) {
             return DOWN;
         }
         return LEFT;
